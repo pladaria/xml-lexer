5a3ba3df1ae0381c51f13192f329e8b108fd1bdf
Pedro Ladaria <pladaria@tuenti.com>
2016-03-25 11:37:01 +0100

    remove debug code

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 const Lexer = require('./src/lexer');
-const State = Lexer.State;
-const Action = Lexer.Action;
 
-const lexer = Lexer.create();
-
-const noop = function () {}; // do nothing
-
-// update state machine
-lexer.stateMachine[State.tagBegin][Action.doubleQuote] = noop;
-lexer.stateMachine[State.tagBegin][Action.lt] = noop;
-
-const xmlWithErrors = `<"<hello>hi</hello attr="value">`;
-
-lexer.on('data', (data) => console.log(data));
-lexer.write(xmlWithErrors);
-
+module.exports = Lexer;

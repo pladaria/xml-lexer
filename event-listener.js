fab684d7dffe1014827181e132f70baf1e90cfe3
Pedro Ladaria <pladaria@tuenti.com>
2016-03-25 12:45:47 +0100

    update event emitter

--- event-listener.js
+++ event-listener.js
@@ -1,10 +1,28 @@
+/**
+ * Quick and dirty implementation of an EventEmitter
+ *
+ * This should work in any platform (node, browser, react native)
+ */
+
+'use strict';
+
+let immediate = null;
+
+if (typeof setImmediate === 'function') {
+    immediate = setImmediate;
+} else if (typeof Promise === 'function' && typeof Promise.resolve === 'function') {
+    immediate = (fn) => Promise.resolve().then(fn);
+} else {
+    immediate = (fn) => setTimeout(fn, 0);
+}
+
 const EventListener = function () {
     this._listeners = {};
 };
 
 EventListener.prototype.emit = function (event, arg) {
     if (Array.isArray(this._listeners[event])) {
-        this._listeners[event].forEach(fn => fn(arg));
+        this._listeners[event].forEach(fn => immediate(() => fn(arg)));
     }
 };
 

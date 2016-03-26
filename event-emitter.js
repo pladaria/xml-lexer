51f8f61c8d30b669e000e1647f38e68589e2a30d
Pedro Ladaria <pladaria@tuenti.com>
2016-03-26 13:54:46 +0100

    call handlers sync

--- event-emitter.js
+++ event-emitter.js
@@ -6,23 +6,13 @@
 
 'use strict';
 
-let immediate = null;
-
-if (typeof setImmediate === 'function') {
-    immediate = setImmediate;
-} else if (typeof Promise === 'function' && typeof Promise.resolve === 'function') {
-    immediate = (fn) => Promise.resolve().then(fn);
-} else {
-    immediate = (fn) => setTimeout(fn, 0);
-}
-
 const EventEmitter = function () {
     this._listeners = {};
 };
 
 EventEmitter.prototype.emit = function (event, arg) {
     if (Array.isArray(this._listeners[event])) {
-        this._listeners[event].forEach(fn => immediate(() => fn(arg)));
+        this._listeners[event].forEach(fn => fn(arg));
     }
 };
 
